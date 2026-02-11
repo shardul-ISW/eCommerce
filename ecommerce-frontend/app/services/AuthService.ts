0179f829b9c50b26c01af95e300483a0cbebc173
Shardul Deshpande <Shardul.Deshpande@insightsoftware.com>
2026-02-11 17:32:51 +0530

    Explicitly convert FormData to JSON object

--- ecommerce-frontend/app/services/AuthService.ts
+++ ecommerce-frontend/app/services/AuthService.ts
@@ -1,10 +1,17 @@
 import apiClient from "~/axios_instance";
 
+function formDataToObject(formData: FormData): Record<string, string> {
+  return Object.fromEntries(formData.entries()) as Record<string, string>;
+}
+
 export async function sendBuyerLoginRequest(formData: FormData) {
-  const response = await apiClient.post("/auth/buyer/login", formData);
+  const response = await apiClient.post(
+    "/auth/buyer/login",
+    formDataToObject(formData),
+  );
   localStorage.setItem("accessToken", response.data.accessToken); //eslint-disable-line
 }
 
 export async function sendBuyerRegisterRequest(formData: FormData) {
-  await apiClient.post("/auth/buyer/register", formData);
+  await apiClient.post("/auth/buyer/register", formDataToObject(formData));
 }
